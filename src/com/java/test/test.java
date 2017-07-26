ddd3bcbd81035e70313f34c022680b4bb6dc7f59
lailai <739156890@qq.com>
2017-07-26 15:56:40 +0800

    java 自增自减运算

---
+++ src/com/java/test/test.java
@@ -0,0 +1,21 @@
+package com.java.test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*int a=5/(3>>2);
+		System.out.println(a);*/
+		
+		int b=1,c;
+		c=b+b++-b--;
+		System.out.println(c);
+		System.out.println(b);
+		
+		System.out.println(5&6); 
+		System.out.println(2 << 3);
+		System.out.println(3 >> 2);
+	}
+
+}
